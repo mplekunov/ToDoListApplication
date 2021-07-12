919fe64e476fc656791aaa661858018bebf2bd13
mplekunov <mplekunov@gmail.com>
2021-07-12 19:50:17 -0400

    Add: copyright comments

--- src/main/java/ucf/assignments/ToDoListApp.java
+++ src/main/java/ucf/assignments/ToDoListApp.java
@@ -6,9 +6,6 @@
 package ucf.assignments;
 
 import javafx.application.Application;
-import javafx.application.Platform;
-import javafx.event.ActionEvent;
-import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
